062c8178f3d0eb0a8342429682e130cbf0c28ec0
anushan1508 <anushan1508@gmail.com>
2021-09-08 02:26:36 +0530

    Displaying count frountend finished

---
+++ tempData.js
@@ -0,0 +1,112 @@
+export default tempData = [
+  {
+    name: "Assignments",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Database Quiz",
+        completed: false,
+      },
+      {
+        title: "Web Tech Quiz",
+        completed: true,
+      },
+      {
+        title: "Data Communications Quiz",
+        completed: false,
+      },
+      {
+        title: "Management Quiz",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Assignments2",
+    color: "#941289",
+    todos: [
+      {
+        title: "Database Quiz",
+        completed: false,
+      },
+      {
+        title: "Web Tech Quiz",
+        completed: true,
+      },
+      {
+        title: "Data Communications Quiz",
+        completed: false,
+      },
+      {
+        title: "Management Quiz",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Assignments3",
+    color: "#941225",
+    todos: [
+      {
+        title: "Database Quiz",
+        completed: false,
+      },
+      {
+        title: "Web Tech Quiz",
+        completed: true,
+      },
+      {
+        title: "Data Communications Quiz",
+        completed: false,
+      },
+      {
+        title: "Management Quiz",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Assignments4",
+    color: "#096066",
+    todos: [
+      {
+        title: "Database Quiz",
+        completed: false,
+      },
+      {
+        title: "Web Tech Quiz",
+        completed: true,
+      },
+      {
+        title: "Data Communications Quiz",
+        completed: false,
+      },
+      {
+        title: "Management Quiz",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Assignments5",
+    color: "#45ZA6D9",
+    todos: [
+      {
+        title: "Database Quiz",
+        completed: false,
+      },
+      {
+        title: "Web Tech Quiz",
+        completed: true,
+      },
+      {
+        title: "Data Communications Quiz",
+        completed: false,
+      },
+      {
+        title: "Management Quiz",
+        completed: true,
+      },
+    ],
+  },
+];
